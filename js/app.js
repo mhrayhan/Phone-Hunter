007a2bc3cb1107bdbf46d37fc6de54d3989e0c5a
mhrayhan <mhrayhan430@gmial.com>
2022-03-01 02:56:23 +0600

    show details

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ const displayPhone = brandName => {
         const div = document.createElement('div');
         div.classList.add('col-md-4');
         div.innerHTML = `
-        <div class="card p-3 border-0 shadow rounded">
+        <div data-bs-toggle="modal" data-bs-target="#phoneDetails"  onclick="getApiById('${phone.slug}')" class="card p-3 border-0 shadow rounded">
             <img src="${phone.image}" class="card-img-top">
             <div class="card-body ">
                 <h5 class="card-title">Brand: ${phone.brand}</h5>
@@ -44,4 +44,30 @@ const displayPhone = brandName => {
         `
         displayPhone.appendChild(div);
     })
+}
+
+// Get API By phone id
+const getApiById = phoneId => {
+    // console.log(phoneId);
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+    .then(res => res.json())
+    .then(data => displayPhoneDetails(data.data));
+}
+// Display Phone Details
+const displayPhoneDetails = phone => {
+    console.log(phone.mainFeatures);
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card p-3 border-0 shadow rounded">
+        <img src="${phone.mainFeatures.image}" class="card-img-top">
+        <div class="card-body ">
+            <h5 class="card-title">Brand: ${phone.brand}</h5>
+            <h6 class="card-title">Name: ${phone.phone_name}</h6>
+            <button type="button" class="btn btn-primary">Phone Details</button>
+        </div>
+    </div>
+    `
+    phoneDetails.appendChild(div);
 }
