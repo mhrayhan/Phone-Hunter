b7db9ef94a8b45a07496cb14305fd6d4ebd00ae8
mhrayhan <mhrayhan430@gmial.com>
2022-03-02 00:28:58 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -1,37 +1,39 @@
-// Get API By Search Text
-
+/* ===========Get API By Search Text=============== */
 const searchPhone = () => {
     const searchInput = document.getElementById('search-input').value;
+    searchInput.value = '';
+    showSpinner('block') //show spinner
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchInput}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhone(data.data));
 }
+//toggle spinner
+const showSpinner = style =>{
+  document.getElementById('spinner').style.display = style;
+}
 
-// Display Search Result
+/*==============Display Search Result=================*/
 const displayPhone = brandName => {
-    // console.log(brandName);
-
     //Show how many result Found
     const qtyText = document.getElementById('qty-text');
     if(brandName.length == 0){
         document.getElementById('display-phone').textContent = '';
         document.getElementById('showAll-btn').style.display = 'none';
+        showSpinner('none')
         qtyText.innerText = 'No Result Found';
         qtyText.style.color = 'red';
         return false;
     }else {
         qtyText.innerText = `${brandName.length} Result Found`;
-        qtyText.style.color = '#00c52b';
+        qtyText.style.color = '#029c24';
     }
 
-    document.getElementById('showAll-btn').style.display = 'block';
+    document.getElementById('showAll-btn').style.display = 'block'; 
 
     const displayPhone = document.getElementById('display-phone');
     displayPhone.textContent = ''; //clear previous search result.
     brandName.slice(0, 20).forEach(phone => {
-        // console.log(phone);
-        
         const div = document.createElement('div');
         div.classList.add('col-md-4');
         div.innerHTML = `
@@ -40,22 +42,23 @@ const displayPhone = brandName => {
             <div class="card-body ">
                 <h5 class="card-title">Model: ${phone.phone_name}</h5>
                 <h6 class="card-title">Brand: ${phone.brand}</h6>
-                <button data-bs-toggle="modal" data-bs-target="#phoneDetails"  onclick="getApiById('${phone.slug}')" type="button" class="btn btn-primary">Phone Details</button>
+                <button data-bs-toggle="modal" data-bs-target="#phoneDetails"  onclick="getApiById('${phone.slug}')" type="button" class="details-btn">Details</button>
             </div>
         </div>
         `
         displayPhone.appendChild(div);
+        showSpinner('none') //hide spinner
     })
 }
 
-// Get API By phone id
+/* ===============Get API By phone id====================  */
 const getApiById = phoneId => {
     // console.log(phoneId);
     fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
     .then(res => res.json())
     .then(data => displayPhoneDetails(data.data));
 }
-// Display Phone Details
+/* =================Display Phone Details===================== */
 const displayPhoneDetails = phone => {
     console.log(phone.others);
     const phoneDetails = document.getElementById('phone-details');
@@ -69,14 +72,14 @@ const displayPhoneDetails = phone => {
         <div class="card-body ">
             <h5 class="card-title">Model: ${phone.name}</h5>
             <h6 class="card-title">Brand: ${phone.brand}</h6>
-            <p class="card-title">Release Date: ${phone.releaseDate}</p>
-            <h5>Main Features</h5><hr>
+            <p class="card-title">Release Date: ${phone.releaseDate ? phone.releaseDate : 'Release Date Not Found' }</p>
+            <h5 class="features">Main Features</h5><hr>
             <p><span>Storage: </span>${phone.mainFeatures.storage}</p>
             <p><span>Display: </span>${phone.mainFeatures.displaySize}</p>
             <p><span>Chipset: </span>${phone.mainFeatures.chipSet}</p>
             <p><span>Memory: </span>${phone.mainFeatures.memory}</p>
             <p><span>Sensor: </span>${phone.mainFeatures.sensors}</p>
-            <h5>Others Features</h5><hr>
+            <h5 class="features">Others Features</h5><hr>
             <p><span>WLAN: </span>${phone.others?.WLAN}</p>
             <p><span>Blutooth: </span>${phone.others?.Bluetooth}</p>
             <p><span>GPS: </span>${phone.others?.GPS}</p>
